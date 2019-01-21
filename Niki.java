63e6950ca43082c14810a8b733328b43fec0ad0b
niki <niki@abv.bg>
2019-01-21 10:46:39 +0200

    kofti promqna

--- Niki.java
+++ Niki.java
@@ -1,5 +1,5 @@
 class Niki {
     public static void main(String...args) {
-        System.out.println("Are be, toq komanden red - ti pensioner li si kyv si!");
+        System.out.println("Tova ne iskam da se vijda!");
     }
 }
