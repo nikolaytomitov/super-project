d9676cde58bc610c6b6ed2f2687bf0dc56dd0e11
niki <niki@abv.bg>
2019-01-21 10:57:02 +0200

    moite promeni

--- Niki.java
+++ Niki.java
@@ -1,5 +1,6 @@
 class Niki {
     public static void main(String...args) {
         System.out.println("Are be, toq komanden red - ti pensioner li si kyv si!");
+        System.out.println("Male tova kak shte go polzvame - ne znam!");
     }
 }
